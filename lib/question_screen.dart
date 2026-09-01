1e90d76c6b20e7fe3b549272f7db368b7b7b0539
Hoang Son <mikechael74@gmail.com>
2026-09-01 15:17:19 +0700

    new update

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:my_first_project/answer_button.dart';
+import 'package:my_first_project/data/questions.dart';
+
 class QuestionScreen extends StatefulWidget {
    const QuestionScreen({super.key});
 
@@ -8,11 +12,26 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
+  final currentQuestion = questions[0];
+
    @override
    Widget build(BuildContext context) {
      return const Scaffold(
-       body: Center(
-         child: Text('Question Screen'),
+       body: SizedBox(
+         width: double.infinity,
+         child: const Column(
+           mainaxisAlignment: MainAxisAlignment.center,
+           children: [
+             Text(
+              currentQuestion.text, 
+              style: const TextStyle(color: Colors.white, fontSize: 24)
+              ),
+             SizedBox(height: 30),
+            ...currentQuestion.answers.map((answer) {
+              return AnswerButton(answerText: answer, onTap: () {});
+            }),
+           ],
+         ), // Column
        ),
      );
    }
